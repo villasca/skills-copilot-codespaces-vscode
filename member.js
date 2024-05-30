7f5f1c9b250763128369d03eae17663a68faa19a
villasca <168598003+villasca@users.noreply.github.com>
2024-05-30 20:47:41 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,10 @@
+function skillsMember() {
+  var skills = {
+    name: "John",
+    age: 30,
+    skills: ["HTML", "CSS", "JS"],
+    salary: 2000,
+  };
+  // return the skills object
+  return skills;
+}
